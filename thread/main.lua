5b7e4dcb1ada860659f56a372aac0d025ad4958c
Ubuntu <ubuntu@ip-172-31-15-116.us-west-2.compute.internal>
2016-03-30 12:53:03 +0000

    ikkw q

--- thread/main.lua
+++ thread/main.lua
@@ -1,4 +1,5 @@
 --dofile("skip_gram.lua")
+require "nn"
 
 config = {}
 config.corpus = "../data/corpus.txt"    -- input file
@@ -52,7 +53,7 @@ parameters["unigram_model_power"] = 0.75
 parameters["label"] = torch.zeros(1 + parameters["neg_sample_number"]);
 parameters["label"][1] = 1
 
-function Split(line, sep)
+local function Split(line, sep)
     local t = {}; local i = 1
     for word in string.gmatch(line, "%a+") do
         t[i] = word; i = i + 1
@@ -60,7 +61,7 @@ function Split(line, sep)
     return t
 end
 
-function BuildVocabulary()
+local function BuildVocabulary()
     local start = sys.clock()
     local file = io.open(parameters["corpus"], "r")
     parameters["vocab"] = {}
@@ -87,8 +88,48 @@ function BuildVocabulary()
     print(string.format("BuildVacabulary costs: %d", sys.clock() - start))
 end
 
+local function BuildTable()
+    local start = sys.clock()
+    local total_count_power = 0
+    for _, count in pairs(parameters["vocab"]) do
+        total_count_power = total_count_power + count^parameters["unigram_model_power"]
+    end
+
+    parameters["table"] = torch.IntTensor(parameters["table_size"])
+    local accum_word_prob = 0; accum_word_index = 0
+    for word, count in pairs(parameters["vocab"]) do
+        accum_word_prob = accum_word_prob + count^parameters["unigram_model_power"] / total_count_power
+        while (accum_word_index + 1) / parameters["table_size"] < accum_word_prob do
+            accum_word_index = accum_word_index + 1
+            parameters["table"][accum_word_index] = parameters["word2index"][word]
+        end
+    end
+    parameters["table_size"] = accum_word_index
+    print(string.format("BuildTable costs: %d", sys.clock() - start))
+end
+
+local function ConstructModel()
+    local skip_gram = nn.Sequential()
+    skip_gram:add(nn.ParallelTable())
+    local word_vector = nn.LookupTable(parameters["vocab_size"], parameters["dim"])
+    word_vector:reset(0.25);
+    local context_vector = nn.LookupTable(parameters["vocab_size"], parameters["dim"])
+    context_vector:reset(0.25)
+    skip_gram.modules[1]:add(word_vector)
+    skip_gram.modules[1]:add(context_vector)
+    skip_gram:add(nn.MM(false, true))
+    skip_gram:add(nn.Sigmoid())
+    local criterion = nn.BCECriterion()
+
+    return skip_gram, criterion
+    -- TODO
+end
+
 if config.mode == "train" then
     BuildVocabulary()
+    BuildTable()
+    local skip_gram, criterion = ConstructModel()
+    print(parameters["table_size"])
 end
 
 
--- thread/test.lua
+++ thread/test.lua
@@ -4,18 +4,29 @@ local nthread = 4
 local njob = 10
 local msg = "hello from a satellite thread"
 
+local t = torch.IntTensor(5)
+t[1] = 3
 
 local pool = threads.Threads(
    nthread,
    function(threadid)
       print('starting a new thread/state number ' .. threadid)
       gmsg = msg -- get it the msg upvalue and store it in thread state
+   end,
+
+   function(threadid)
+      local m = t
+
+      function g(threadid)
+         print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ' .. threadid)
+         print('g(threadid): ' .. m[1])
+      end
    end
+
 )
 
-local function g(threadid)
- print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ' .. threadid)
-end
+
+--local tt = t;
 
 local jobdone = 0
 for i=1,njob do
@@ -27,7 +38,8 @@ for i=1,njob do
       end,
 
       function(id)
-         print(string.format("task %d finished (ran on thread ID %x)", i, id))
+         t[1] = id
+         print(string.format("task %d finished (ran on thread ID %x), t[1] == %d", i, id, t[1]))
          jobdone = jobdone + 1
       end
    )
